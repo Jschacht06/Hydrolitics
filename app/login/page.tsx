29ad2db41c8175fcea296c0c79d1c8feaa5b0cbe
Juha Schacht <125817333+Jschacht06@users.noreply.github.com>
2025-07-07 22:43:45 +0200

    added login page frontend

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,52 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 export default function Login() {
-  return <div>About</div>
+  return ( 
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+  <Card className="w-full max-w-sm flex">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+      </CardFooter>
+    </Card>
+    </div>
+  )
 }
